89b663d740020856ef11cdf0a719830d5e802101
Palito <axelllobet76@gmail.com>
2025-05-26 16:45:55 -0300

    1.4

--- app.js
+++ app.js
@@ -2,15 +2,19 @@ import express, { json } from 'express' // require -> commonJS
 import { createMovieRouter } from './routes/movies.js'
 import "dotenv/config"
 import { corsMiddleware } from './middlewares/cors.js'
+import { MovieModel } from './models/mysql/movie.js'
 
-export const createApp = ({movieModel}) =>{
   
-  const app = express()
-  app.use(json())
-  app.use(corsMiddleware())
-  app.disable('x-powered-by')
+const app = express()
+app.use(json())
+app.use(corsMiddleware())
+app.disable('x-powered-by')
 
-  app.use('/movies', createMovieRouter({movieModel: movieModel}))
+app.use('/movies', createMovieRouter({movieModel: MovieModel}))
 
-  return app
-}
+const PORT = process.env.PORT ?? 1234
+
+app.listen(PORT, () => {
+  console.log(`server listening on port http://localhost:${PORT}`)
+})  
+  
