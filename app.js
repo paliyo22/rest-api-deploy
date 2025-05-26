7c69a432ec69bb572eada3ddb0fb19586aeafeb0
Palito <axelllobet76@gmail.com>
2025-05-26 16:28:55 -0300

    1.3

--- app.js
+++ app.js
@@ -12,9 +12,5 @@ export const createApp = ({movieModel}) =>{
 
   app.use('/movies', createMovieRouter({movieModel: movieModel}))
 
-  const PORT = process.env.PORT ?? 1234
-
-  app.listen(PORT, () => {
-    console.log(`server listening on port http://localhost:${PORT}`)
-  })  
+  return app
 }
--- server-with-mysql.js
+++ server-with-mysql.js
@@ -1,4 +1,10 @@
 import { createApp } from "./app.js";
 import { MovieModel } from "./models/mysql/movie.js";
 
-createApp({movieModel: MovieModel})
+const app = createApp({movieModel: MovieModel})
+
+const PORT = process.env.PORT ?? 1234
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`)
+})
